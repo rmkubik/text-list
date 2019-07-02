dd9b629deca4d327b602f2027aa7661a6f1bb87f
Ryan Kubik <rkubik@twilio.com>
2019-07-02 15:56:40 -0700

    Update Gatsby

--- tasks/create-env-file.js
+++ tasks/create-env-file.js
@@ -4,4 +4,4 @@ const entries = Object.entries(process.env);
 const array = entries.map(([key, value]) => `${key}=${value}`);
 const env = array.join('\n');
 
-fs.writeFile('./env.production', env, console.log);
+fs.writeFile('./env.production', env, () => console.log(env));
