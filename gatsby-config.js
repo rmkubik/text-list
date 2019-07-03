394e9c6d1d739d056feb139d472f258a0c17264c
Ryan Kubik <rkubik@twilio.com>
2019-07-03 09:14:16 -0700

    Try adding dotenv without GATSBY_

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+// TODO: Try this suggestion by @joybear in Gatsby Discord to enable
+// us to use environment variables without the GATSBY_ prefix in Netlify.
+require('dotenv').config();
+
 /**
  * Configure your Gatsby site with this file.
  *
@@ -6,4 +10,4 @@
 
 module.exports = {
   /* Your site config here */
-}
+};
--- tasks/create-env-file.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const entries = Object.entries(process.env);
-const array = entries.map(([key, value]) => `${key}=${value}`);
-const env = array.join('\n');
-
-fs.writeFile('./env.production', env, () => console.log(env));
