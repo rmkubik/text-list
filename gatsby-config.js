48f714d47792f629d02445a67af26657e693dc92
Ryan Kubik <rkubik@twilio.com>
2019-07-03 09:16:46 -0700

    ADd back GATSBY_ prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,3 @@
-// TODO: Try this suggestion by @joybear in Gatsby Discord to enable
-// us to use environment variables without the GATSBY_ prefix in Netlify.
-require('dotenv').config();
-
 /**
  * Configure your Gatsby site with this file.
  *
