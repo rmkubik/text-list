b968f2d2f5009f1ab20c2bb9bb09cc2e9730a691
Ryan Kubik <rkubik@twilio.com>
2019-07-03 15:42:58 -0700

    Auth test for test endpoint

--- src/functions/test.js
+++ src/functions/test.js
@@ -1,10 +1,35 @@
 exports.handler = function(event, context, callback) {
-  // your server-side functionality
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      event,
-      context,
-    }),
+  const jwksClient = require('jwks-rsa');
+
+  const client = jwksClient({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 10, // Default value
+    jwksUri: 'https://dev-c15ae8r6.auth0.com/.well-known/jwks.json',
+  });
+
+  // gotten from auth0 jwks.json
+  const kid = 'RDE5N0Q1NTEwMEUyQTUyRDY4NzcwMzhENTdERjQ2MjUxNTc3N0IyQQ';
+
+  client.getSigningKey(kid, (err, key) => {
+    const signingKey = key.publicKey || key.rsaPublicKey;
+
+    const headerType = 'Authorization: Bearer ';
+    const token = event.headers.authorization.slice(headerType.length);
+
+    const decoded = jwt.verify(token, signingKey);
+
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          event,
+          context,
+          decoded,
+        },
+        undefined,
+        2
+      ),
+    });
   });
 };
